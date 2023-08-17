7f74038a0314df64d723ca952ffae82cec95acb3
Pascu Ioana-Georgia <ioana.pascu@history.bucharest.university>
2023-08-17 13:06:38 +0300

    going back to old path, images not imporeyd

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -13,15 +13,15 @@ const data = reactive({
 const styles = {
     'rock': {
         class: 'rockBg',
-        img: 'icon-rock.svg'
+        img: '../images/icon-rock.svg'
     },
     'paper': {
         class: 'paperBg',
-        img: 'icon-paper.svg'
+        img: '../images/icon-paper.svg'
     },
     'scissors': {
         class: 'scissorsBg',
-        img: 'icon-scissors.svg'
+        img: '../images/icon-scissors.svg'
     }
 }
 
